7dbbbf0b754b296d9ef92333ea4f85f2f4e2084a
Larry Beall <lawrence.beall@gmail.com>
2012-03-18 00:57:28 -0400

    Added topic property to Room Model.

--- JabbR.Client/Models/Room.cs
+++ JabbR.Client/Models/Room.cs
@@ -7,6 +7,7 @@ namespace JabbR.Client.Models
         public string Name { get; set; }
         public int Count { get; set; }
         public bool Private { get; set; }
+        public string Topic { get; set; }
         public IEnumerable<User> Users { get; set; }
         public IEnumerable<string> Owners { get; set; }
         public IEnumerable<Message> RecentMessages { get; set; }
